e2edad3e51303303af176ada9ee2137b293f2a35
Rodolfo Barrios <rodolfo.9408@gmail.com>
2019-06-10 16:54:39 -0500

    Subiendo proyecto base

---
+++ src/test/java/com/malfor/integracion/IntegracionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.malfor.integracion;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IntegracionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
